d70e2398dcf61d1b15d92e48989a06bc09399e92
yushi <y2831451@gmail.com>
2024-10-01 16:50:51 +0900

    change probability

--- sketch.js
+++ sketch.js
@@ -3,9 +3,14 @@ let rows = 25;  // 縦（行）数
 let cols = 20;  // 横（列）数
 let cellSize = 20;  // セルの初期サイズ
 let minCellSize = 1;  // セルサイズの最小値
+let p = 0.05;               // 確率
 let grid = new Array(rows);  // 行数分の配列を作成
 let maxCanvasWidth = 500;
 let maxCanvasHeight = 600;
+
+let showText = true;  // テキストを表示するかどうかのフラグ
+let textStartTime = 0;  // テキスト表示開始時刻を記録
+let displayDuration = 10000;  // テキストを表示する時間（10秒 = 10000ミリ秒）
 
 for (let i = 0; i < rows; i++) {
     grid[i] = new Array(cols);  // 各行に列数分の配列を追加
@@ -45,8 +50,8 @@ function draw() {
             // 配列の値を半径とする
             let r = grid[y][x];
 
-            // セルが最大サイズに達していない場合、5%の確率で半径を増加
-            if (r < cellSize && random(1) < 0.1) {
+            // セルが最大サイズに達していない場合、pの確率で半径を増加
+            if (r < cellSize && random(1) < p) {
                 grid[y][x] += random(1, 5);  // 半径の増加を調整
             }
 
@@ -73,8 +78,19 @@ function draw() {
     // 全てのセルが最大サイズに達したら、セルサイズを縮小
     if (allCellsMaxed) {
         cellSize -= 3;  // セルサイズを減らす
+        if (cellSize <= 11) {
+            p = 0.1;
+        } else if (cellSize <= 8) {
+            p = 0.2;
+        } else if (cellSize <= 5) {
+            p = 0.4;
+        } else if (cellSize <= 2) {
+            p = 0.8;
+        }
+        
         if (cellSize < minCellSize) {
             cellSize = 20;  // セルサイズが最小値を下回ったら再度20にリセット
+            p = 0.05  // pを初期値に戻す
         }
 
         // rows と cols の更新
@@ -83,6 +99,12 @@ function draw() {
 
         // グリッドの行数と列数を更新し、再初期化
         setupGrid();
+    }
+    if (cellSize <= 2) {
+        textSize(32);  // テキストサイズを設定
+        fill(255, 0, 0);  // 青で描画
+        textAlign(CENTER, TOP);  // 上中央にテキストを配置
+        text("真珠の耳飾りの少女", width / 2, 20);  // テキストを表示
     }
 }
 
